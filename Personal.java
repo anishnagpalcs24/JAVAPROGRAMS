eb81be6a2006d8f61e1c3969ee106841481a63fc
anishnagpalcs24 <anishnagpal.cs24@bmsce.ac.in>
2025-12-19 00:16:35 +0530

    Add files via upload

---
+++ Personal.java
@@ -0,0 +1,13 @@
+package CIE;
+
+public class Personal {
+    protected String USN;
+    protected String name;
+    protected int sem;
+
+    public Personal(String USN, String name, int sem) {
+        this.USN = USN;
+        this.name = name;
+        this.sem = sem;
+    }
+}
